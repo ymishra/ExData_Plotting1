98258e3ccd9ae8ee75419a9ffde534b904f4b553
ymishra <yashi.mishra@me.com>
2016-06-06 00:06:12 -0700

    updatetoAssignment1

--- Plot2.R
+++ Plot2.R
@@ -3,7 +3,7 @@ subsetData <- test[test$Date %in% c("1/2/2007", "2/21/2007")]
 GlobalActivePower <- as.numeric(subsetData$Global_active_power)
 Datetime1 <- paste(subsetData$Date, subsetData$Time, sep = " ")
 Datetime <- strptime(Datetime1,"%d/%m/%Y %H:%M:%S")
-plot(Datetime, GlobalActivePower, type="l", xlab="", ylab="Global Active Power(kilowatts)")
+plot(Datetime, GlobalActivePower, type="l", xlab="DateTime", ylab="Global Active Power(kilowatts)")
 
 
 
